99d85939fa7428825471fcc4c8925d1bd49402ec
ClockZhong <55649441+0utsiderZhong@users.noreply.github.com>
2023-07-25 23:33:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ GOOGLE_DRIVE_SCOPE = 'https://www.googleapis.com/auth/drive'
 # max asyncio tasks number, you can check the Google Drive api limit at: https://developers.google.com/drive/labels/limits
 # Write requests	Per user per project	300 (queries per second)
 # Attention: This parameter can not prevent the creation of task, it just makes sure that 100 tasks running at the same time
-# If you have 1 million files to upload, 1 million takes will be created, it will take lots of memory in your server
+# If you have 1 million files to upload, 1 million takes will be created
 # Each simple task will allocate 2kb, it will use at least 2GB of RAM in your server
 # Reference: https://www.clockblog.life/article/2023/7/3/39.html#_2
 MAX_CONCURRENCY_NUM = 100
